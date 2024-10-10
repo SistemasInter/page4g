31a9ec50c2218fee76fa86acd8108def4fc8bb8c
SistemasInter <sistemas@interenlace.com.mx>
2024-10-10 12:17:24 -0600

    feat: add navbar functionality and redesign

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,13 +32,40 @@ document.addEventListener("DOMContentLoaded", function () {
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-            arrows: false,
+          arrows: false,
           centerPadding: "50px",
         },
       },
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ],
+  });
+
+  // Manejar el evento de desplazamiento
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+
+  window.addEventListener("scroll", () => {
+    let current = "";
+
+    // El usuario está en la parte superior de la página
+    if (pageYOffset === 0) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+      });
+      return;
+    }
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      if (pageYOffset >= sectionTop - 60) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach((link) => {
+      link.classList.remove("active");
+      if (link.getAttribute("href").includes(current)) {
+        link.classList.add("active");
+      }
+    });
   });
 });
